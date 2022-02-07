8242dfe39177058a5eec7f764b1e3dbd3bcf4b7d
ArtemKudryavcev <himynameisartem@icloud.com>
2022-02-07 16:16:38 +0300

    Initial Commit

---
+++ Try Post Request in NaSpote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Try Post Request in NaSpote
+//
+//  Created by Артем Кудрявцев on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
